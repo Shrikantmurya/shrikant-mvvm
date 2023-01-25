3e6579d43afa5196d237aa362619ecb71ee7f86a
Shrikant Maurya <57619167+Shrikantmurya@users.noreply.github.com>
2023-01-25 21:14:55 +0530

    update header pass

--- lib/data/network/NetworkApiService.dart
+++ lib/data/network/NetworkApiService.dart
@@ -4,17 +4,22 @@ import 'package:http/http.dart';
 import '/data/app_excaptions.dart';
 import '/data/network/BaseApiServices.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NetworkApiService extends BaseApiServices {
   @override
   Future getGetApiResponse(String url) async {
+    final prefsToken = await SharedPreferences.getInstance();
+    dynamic token = prefsToken.getString('token');
     dynamic responseJson;
     try {
-      final response =
-          await http.get(Uri.parse(url), headers: {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-        },).timeout(const Duration(seconds: 120));
+      Map<String, String> headerData = {"Authorization": token};
+      final response = await http
+          .get(
+            Uri.parse(url),
+            headers: headerData,
+          )
+          .timeout(const Duration(seconds: 10));
       responseJson = returnResponse(response);
     } on SocketException {
       throw FetchDataException('No Internet Connection');
@@ -24,16 +29,40 @@ class NetworkApiService extends BaseApiServices {
   }
 
   @override
-  Future getPostApiResponse(String url, dynamic data) async {
+  Future getPostApiResponse(
+    String url,
+    dynamic data,
+  ) async {
+    final prefsToken = await SharedPreferences.getInstance();
+    dynamic token = prefsToken.getString('token');
     dynamic responseJson;
     try {
-      Response response = await post(Uri.parse(url),
-       headers: {
-          'Content-type': 'application/json',
-          'Accept': 'application/json',
-        },
-       body: data)
-          .timeout(Duration(seconds: 120));
+      Map<String, String> headerData = {"Authorization": token};
+      Response response = await post(
+        Uri.parse(url),
+        headers: headerData,
+        body: data,
+      ).timeout(Duration(seconds: 10));
+
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+
+    return responseJson;
+  }
+
+  @override
+  Future getPostLoginApiResponse(
+    String url,
+    dynamic data,
+  ) async {
+    dynamic responseJson;
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        body: data,
+      ).timeout(Duration(seconds: 10));
 
       responseJson = returnResponse(response);
     } on SocketException {
